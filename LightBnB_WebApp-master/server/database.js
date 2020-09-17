f4b6d49c5964f360d229e5717aaf647e31e62928
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-09-17 20:42:06 +0000

    modified getAllProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -84,19 +84,56 @@ exports.getAllReservations = getAllReservations;
 /// Properties
 
 /**
- * Get all properties.
+ * Get all properties.q
+ * 
  * @param {{}} options An object containing query options.
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT properties.*, avg(property_reviews.rating) as average_rating 
-  FROM property_reviews
-  JOIN properties ON properties.id = property_id
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length}`;
+  }
+  
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += ` AND owner_id = $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    queryString += ` AND cost_per_night > $${queryParams.length}`;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    queryString += ` AND cost_per_night < $${queryParams.length}`;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` AND rating >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
   GROUP BY properties.id
-  LIMIT $1
-  `, [limit])
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length}
+  `;
+
+  console.log(queryString, queryParams, limit);
+
+  return pool.query(queryString, queryParams)
   .then(res => res.rows)
   .catch(err => console.log(err));
 }
